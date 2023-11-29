3ae6a40558d048cdd3ece37ca49e76fe4889ad11
gracemora <moragrac@msu.edu>
2023-11-28 21:15:15 -0500

    Check if changes push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:818b107cd1dd88896d05de642b989fdba30266787de743305e1e259d7b34711d
-size 5534
+oid sha256:f583cb910a05885364ff4f253b1d2a4880f0b2be69f162ea86f2b98fb1970631
+size 5542
