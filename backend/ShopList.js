a8dbd3aa3fd93ffadb041e6870b238fcb7d060e1
gracemora <moragrac@msu.edu>
2023-11-28 21:41:57 -0500

    Check changes

--- backend/ShopList.js
+++ backend/ShopList.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b859761bb7aedd057970648860f78d219db4bc8e4322575f1922f43c530cd1f3
-size 1866
+oid sha256:64ab14ef32b7d57e5e6efecda72b19e4ca1929b77726b150eccf1839ecddb168
+size 1884
