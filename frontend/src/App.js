705e7734e91436ef2ae52492f691b977fc3bfae9
gracemora <moragrac@msu.edu>
2023-11-30 13:53:36 -0500

    Removed Contact page for time being

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f583cb910a05885364ff4f253b1d2a4880f0b2be69f162ea86f2b98fb1970631
-size 5542
+oid sha256:a85839aa4fe0f4176270c366263cd85eb4f806de7b4fbc8f8278d5573c620805
+size 6027
---
+++ frontend/src/components/ProtectedRoute.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b5c6c2c16ed8ec087d32270d30d221b79b730e1adfdc4286d55a84b32459398
+size 307
